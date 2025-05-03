6976a360fa4a4e1ac5f07371a7e8cecf26d9883c
EnochJasonJ <enochjason06@gmail.com>
2025-05-03 21:26:00 +0530

    Updated views.py

--- app/views.py
+++ app/views.py
@@ -4,18 +4,12 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from django.conf import settings
-from transformers import BertForSequenceClassification, BertTokenizer
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-
-# model = BertForSequenceClassification.from_pretrained('app/my_model')
 model_name = "enoch10jason/spam-detector-model"
-# tokenizer = BertTokenizer.from_pretrained('app/my_model')
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
 model.eval()
-
-
 
 class CheckSpam(ListCreateAPIView):
     def post(self, request, *args, **kwargs):
@@ -26,20 +20,14 @@ class CheckSpam(ListCreateAPIView):
         if not message:
             return Response({'error': 'No message provided'}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Tokenize the input message
         inputs = tokenizer(message, return_tensors="pt", truncation=True, padding=True, max_length=512)
-
-        # Move input tensors to the same device as the model
         inputs = {key: value.to(device) for key, value in inputs.items()}
 
-        # Perform inference
         with torch.no_grad():
             outputs = model(**inputs)
 
-        # Get prediction
         prediction = torch.argmax(outputs.logits, dim=-1).item()
 
-        # Return result
         if prediction == 0:
             return Response({'message': 'Spam'}, status=status.HTTP_200_OK)
         else:
