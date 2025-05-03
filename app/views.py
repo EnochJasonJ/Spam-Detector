6722951cbbf0cd1c1c00c3b8d26e12f644c220ca
EnochJasonJ <enochjason06@gmail.com>
2025-05-03 20:53:46 +0530

    Fixed torch

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
-import torch
+
 from django.conf import settings
 from transformers import BertForSequenceClassification, BertTokenizer
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
@@ -15,11 +15,13 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 model.eval()
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
+
 
 class CheckSpam(ListCreateAPIView):
     def post(self, request, *args, **kwargs):
+        import torch
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model.to(device)
         message = request.data.get('message', '')
         if not message:
             return Response({'error': 'No message provided'}, status=status.HTTP_400_BAD_REQUEST)
