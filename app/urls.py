9f8b88ee3497b745fc858750980a6e3af5bf55cb
EnochJasonJ <enochjason06@gmail.com>
2025-05-03 19:48:31 +0530

    Updated urls.py in app

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from django.shortcuts import redirect
 from .views import CheckSpam
 
+def redirect_to_checkspam(request):
+    return redirect('/checkspam/')
+
+
 urlpatterns = [
+    path('', redirect_to_checkspam, name='root_redirect'),
     path('checkspam/', CheckSpam.as_view(), name='check_spam'),
 ]
